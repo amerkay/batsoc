3e491cf133302e08fb9b42958422de16f4222711
Amer <amerk86@gmail.com>
2021-03-23 00:52:20 +1000

    Tailwind CSS plugins and Netlify config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,25 +6,24 @@ export default {
   head: {
     title: 'batsoc',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -45,6 +44,5 @@ export default {
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,34 @@
-/*
- ** TailwindCSS Configuration File
- **
- ** Docs: https://tailwindcss.com/docs/configuration
- **
- ** DEFAULTS: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-
- ** Get shades: https://javisperez.github.io/tailwindcolorshades/#/ 
- **
- ** TODO: Palette https://coolors.co/123478-ff645c-ffffff-24cf73-fbe74b-166bb6-cc0a00
- **
- **   Colors: navy-500, pink-500, green-500, yellow-500, orange-500, green-500
- */
-
-// const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
+  // Extends the default Studio Meta Tailwind configuration here...
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        // primary: defaultTheme.colors.green,
-        primarygreen: {
-          50: '#f8fcfc',
-          100: '#f1f9f9',
-          200: '#ddf1ef',
-          300: '#c8e9e5',
-          400: '#9ed8d2',
-          500: '#75c7be',
-          600: '#69b3ab',
-          700: '#58958f',
-          800: '#467772',
-          900: '#39625d',
-        },
-        primaryblue: {
-          50: '#f8fbfe',
-          100: '#f1f8fd',
-          200: '#dbedf9',
-          300: '#c5e1f5',
-          400: '#9acbee',
-          500: '#6eb5e7',
-          600: '#63a3d0',
-          700: '#5388ad',
-          800: '#426d8b',
-          900: '#365971',
-        },
-      },
-    },
+    extend: {},
   },
-  variants: {},
-  plugins: [require('@tailwindcss/forms')],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // Docs https://github.com/tailwindlabs/tailwindcss-forms
+    require('@tailwindcss/forms'),
+    // Docs https://github.com/tailwindlabs/tailwindcss-typography
+    require('@tailwindcss/typography'),
+  ],
   purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
+
+    // If you really want to remove all unused styles, use the mode: 'all'
+    // option and be very careful to provide the paths to all files that might
+    // reference any classes or HTML elements
+    mode: 'all',
     content: [
       'components/**/*.vue',
+      'content/**/*.md',
       'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
+      'utils/**/*.js',
       'nuxt.config.js',
-      // TypeScript
-      'plugins/**/*.ts',
-      'nuxt.config.ts',
     ],
   },
 }
