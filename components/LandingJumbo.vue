7f1aa20970c5f5bc61efe7dfcf625b557ac7b1d8
Amer <amerk86@gmail.com>
2021-03-23 14:22:56 +1000

    Added Footer

---
+++ components/LandingJumbo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="relative bg-gray-50">
+    <main class="lg:relative">
+      <div
+        class="w-full pt-16 pb-20 mx-auto text-center max-w-7xl lg:py-48 lg:text-left"
+      >
+        <div class="px-4 lg:w-7/12 sm:px-8 xl:pr-16">
+          <h1
+            class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
+          >
+            <span class="block xl:inline"> Help us save the endangered </span>
+            <span class="block text-orange-600 xl:inline">
+              Spectacled Flying Fox
+            </span>
+          </h1>
+          <p
+            class="max-w-md mx-auto mt-3 text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl"
+          >
+            Help us save the endangered Spectacled Flying Fox, an essential
+            pollinator and seed disperser for the wet tropics! We are volunteers
+            working with locals legislators to protect them.
+          </p>
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a
+                href="#"
+                class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-orange-600 border border-transparent rounded-md hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
+              >
+                Join us on Patreon
+              </a>
+            </div>
+            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-orange-600 bg-white border border-transparent rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              >
+                Learn more about us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-5/12 lg:h-full"
+      >
+        <img
+          class="absolute inset-0 object-cover w-full h-full"
+          src="../static/img/fox1.webp"
+          alt=""
+        />
+      </div>
+    </main>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <navbar />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
